982e2fc33f8babee532307af9e7e7609ace0394d
anatol-blackforest <anatolblackforest@gmail.com>
2018-07-22 11:40:14 +0300

    return authenticate to each route

--- routes/analytics.js
+++ routes/analytics.js
@@ -3,8 +3,7 @@ const passport = require('passport')
 const controller = require('../controllers/analytics')
 const router = express.Router()
 
-router.all(passport.authenticate('jwt', {session: false}))
-router.get('/overview', controller.overview)
-router.get('/analytics', controller.analytics)
+router.get('/overview', passport.authenticate('jwt', {session: false}), controller.overview)
+router.get('/analytics', passport.authenticate('jwt', {session: false}), controller.analytics)
 
 module.exports = router
--- routes/order.js
+++ routes/order.js
@@ -3,8 +3,7 @@ const passport = require('passport')
 const controller = require('../controllers/order')
 const router = express.Router()
 
-router.all(passport.authenticate('jwt', {session: false}))
-router.get('/', controller.getAll)
-router.post('/', controller.create)
+router.get('/', passport.authenticate('jwt', {session: false}), controller.getAll)
+router.post('/', passport.authenticate('jwt', {session: false}), controller.create)
 
 module.exports = router
